00edaa00fdbfff99165e5ff385918332d3fb1b3b
takrim1999 <takrimulalom@gmail.com>
2025-04-24 11:36:47 +0600

    complete from lab

---
+++ project/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<div class = "starter">
+
+<?php
+echo "<ol start='0'>";
+
+for($i=0;$i<10;$i++){
+    
+    echo "<li> hello </li>";
+
+}
+
+echo "</ol>";
+
+?>
+</div>
+
+</body>
+
+</html>
